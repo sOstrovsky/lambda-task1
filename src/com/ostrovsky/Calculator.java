b01077b8f139bf6ab6ab6c01f8b83d40a71d9f13
Sergey Ostrovsky <sergey.ostrovsky@nordigy.ru>
2022-04-05 01:26:18 +0400

    [task1] code review fixes

--- src/com/ostrovsky/Calculator.java
+++ src/com/ostrovsky/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) -> x / y;
+    BinaryOperator<Integer> divide = (x, y) -> y > 0 ? x / y : 0;
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
--- src/com/ostrovsky/Main.java
+++ src/com/ostrovsky/Main.java
@@ -4,11 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
-        calc.divide = (x, y) -> y > 0 ? x / y : 0;
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1,1);
         /*
-        в варианте из задания нет проверки на то, что делителоьне может быть равен 0
+        в варианте из задания нет проверки на то, что делитель не может быть равен 0
         в моей имплементации в случае деления на 0 вернется 0
         */
         int c = calc.divide.apply(a, b);
